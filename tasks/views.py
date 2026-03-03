9dda1b5aa22807fa37f653bcb785e5c2883e4375
Sifat Mahmud <sifatmahmud5293@gmail.com>
2026-03-03 11:52:53 +0600

    some query practised 7.5

--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from tasks.forms import TaskForm, TaskModelForm
 from tasks.models import Employee, Task, TaskDetail, Project
-from datetime import date
+from datetime import date, timedelta
+from django.utils import timezone
 from django.db.models import Q, Count, Max, Min, Avg
 
 
@@ -41,5 +42,48 @@ def create_task(request):
 
 
 def show_task(request):
-    projects = Project.objects.annotate(num_task=Count('task')).order_by('num_task')
-    return render(request, 'show_task.html', {'projects': projects})
+    # Show the tasks which are assigned to a specific employee
+    # employee = Employee.objects.get(id=1)
+    # tasks = employee.tasks.all()
+    # return render(request, 'show_task.html', {'tasks': tasks})
+
+    # Show all employees working on a specific project
+    # project = Project.objects.get(id=3)
+    # employees = Employee.objects.filter(tasks__project=project).distinct()
+    # return render(request, 'show_task.html', {'employees': employees})
+
+    # Get all tasks that are due today
+    # tasks = Task.objects.filter(due_date=date.today())
+    # return render(request, 'show_task.html', {'tasks': tasks})
+
+
+    # Show all tasks with a priority higher than 'low'
+    # tasks = TaskDetail.objects.exclude(priority='L')
+    # return render(request, 'show_task.html', {'tasks': tasks})
+
+
+    # employee = Employee.objects.get(id=2)
+    # tasks_count = employee.tasks.all().count()
+    # return render(request, 'show_task.html', {'tasks_count': tasks_count})
+
+
+    # recent_task = Task.objects.order_by('created_at').first()
+    # return render(request, 'show_task.html', {'recent_task': recent_task})
+
+
+
+    # projects = Project.objects.annotate(task_count = Count('task')).filter(task_count = 2)
+    # return render(request, 'show_task.html', {'projects': projects})
+
+    # now = timezone.now()
+    # one_week_ago = now - timedelta(days=7)
+    # overdue_tasks = Task.objects.filter(is_completed=False, due_date__lt=one_week_ago)
+    # return render(request, 'show_task.html', {'tasks': overdue_tasks})
+
+
+    # employees_with_task_count = Employee.objects.annotate(total_tasks=Count('tasks'))
+    # return render(request, 'show_task.html', {'emps': employees_with_task_count})
+
+
+    tasks = Task.objects.exclude(status='PENDING')
+    return render(request, 'show_task.html', {'tasks': tasks})
